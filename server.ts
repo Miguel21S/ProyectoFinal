a5a353c6b479f68e7b5ff8796e4c6f7360e39038
Miguel21S <leaismiguelss@gamail.com>
2024-04-26 16:41:30 +0200

    Creación y Actualización: server y pacage.json

---
+++ server.ts
@@ -0,0 +1,16 @@
+
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 2099;
+
+app.use(express.json());
+
+app.get("api/healthy", (req, res) => {
+    res.status(200).json({ success: true, message: "server is healthy" })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
