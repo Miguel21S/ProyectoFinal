4d19343174419dc4da3968fcd66b12835bc69660
Miguel21S <leaismiguelss@gamail.com>
2024-05-16 20:57:16 +0200

    Actualización: método eliminar reserva de alojamiento

--- src/entities/reservaAlojamientos/router.ts
+++ src/entities/reservaAlojamientos/router.ts
@@ -8,8 +8,8 @@ import { isSuperAdmin } from "../../core/middlewares/isSuperAdministrador";
 const router = Router();
 
 router.post('/crear/reserva/:id', auth, reservaAlojamiento.crearReservaAlojamiento);
-router.get('/reserva', auth, isSuperAdmin, reservaAlojamiento.listarReservaAlojamiento);
-router.get('/mis/reserva', auth, reservaAlojamiento.misReservarAlojamiento);
+router.get('/reserva/admin', auth, isSuperAdmin, reservaAlojamiento.listarReservaAlojamientoAdmin);
+router.get('/mis/reserva/usuario', auth, reservaAlojamiento.misReservarAlojamiento);
 router.put('/actualizar/reserva/:id', auth, reservaAlojamiento.editarReservaAlojamiento);
 router.delete('/eliminar/reserva/:id', auth, reservaAlojamiento.eliminarReservaAlojamiento);
 
